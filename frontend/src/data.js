a09bb5bbe481d3614d7f85d48407fd427a1a209a
Mayank <mayankkrishna96@gmail.com>
2021-04-30 13:33:40 +0530

    Basic skeleton of the Frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Bamboo flute',
+            category: 'Woodwind Instruments',
+            image: '/images/p1.jpeg',
+            price: 200,
+            brand: 'HariPrasad',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Beautiful C Scale Flute',
+        },
+
+        {
+            _id: '2',
+            name: 'Sitar',
+            category: 'String Instruments',
+            image: '/images/p2.jpeg',
+            price: 3000,
+            brand: 'Ravi',
+            rating: 4.5,
+            numReviews: 10,
+            description: '4 strings Sitar with bass',
+        },
+
+        {
+            _id: '3',
+            name: 'Bamboo flute',
+            category: 'Musical Instruments',
+            image: '/images/p1.jpeg',
+            price: 700,
+            brand: 'HariPrasad',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Beautiful C Scale Flute',
+        },
+
+        {
+            _id: '4',
+            name: 'Sitar',
+            category: 'String Instruments',
+            image: '/images/p2.jpeg',
+            price: 3500,
+            brand: 'Ravi',
+            rating: 4.5,
+            numReviews: 10,
+            description: '4 strings Sitar with bass',
+        },
+
+        {
+            _id: '5',
+            name: 'Bamboo flute',
+            category: 'Woodwind Instruments',
+            image: '/images/p1.jpeg',
+            price: 500,
+            brand: 'HariPrasad',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Beautiful C Scale Flute',
+        },
+    ],
+};
+
+export default data;
